d55f40cc30746ec459476d4f855c6d792795d93a
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-29 09:34:01 +0200

    functions btn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict";
+// 1. Elements we need from the HTML
+const btn = document.querySelector(".js_btn");
+
+// 2. Declare functions and events
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  console.log("Hola, has hecho click!");
+});
+/* 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  } */
+
+// 3. Code that runs when the page loads
