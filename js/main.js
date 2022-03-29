9ceedec85365745d45b54f9c005457ff728d4c3f
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-29 10:42:28 +0200

    aleatori number, select number and change text

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 "use strict";
 // 1. Elements we need from the HTML
 const btn = document.querySelector(".js_btn");
+const select = document.querySelector(".js_select");
+const resultText = document.querySelector(".js_changeText");
 
 // 2. Declare functions and events
 
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
 btn.addEventListener("click", (event) => {
   event.preventDefault();
-
-  console.log("Hola, has hecho click!");
+  const valueFromThePlayer = parseInt(select.value);
+  const randomNumber = getRandomNumber(6);
+  if (valueFromThePlayer === randomNumber) {
+    resultText.innerHTML = "Has ganado el doble de lo apostado :)";
+  } else {
+    resultText.innerHTML = "Has perdido lo apostado :(";
+  }
 });
-/* 
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  } */
-
-// 3. Code that runs when the page loads
