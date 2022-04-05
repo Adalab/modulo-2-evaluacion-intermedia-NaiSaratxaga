ec5eae1615ad3ce74c4f60a1d3e2c8ec26a9d6c6
NaiSaratxaga <nsaratxaga@gmail.com>
2022-04-05 23:43:46 +0200

    Improves the code based on the issues

--- js/main.js
+++ js/main.js
@@ -1,41 +1,73 @@
-"use strict";
-// bonus
-let userMoney = 50;
+'use strict';
 
 // 1. Elements we need from the HTML
-const btn = document.querySelector(".js_btn");
-const select = document.querySelector(".js_select");
-const resultText = document.querySelector(".js_changeText");
-const playButton = document.querySelector(".js_btn");
-const resetButton = document.querySelector(".js_reset");
-const moneyInput = document.getElementById("money");
-const userMoneyText = document.getElementById("user_money");
+const numberSelect = document.querySelector('.js_select');
+const bettingAmountInput = document.querySelector('.js_money');
 
-userMoneyText.innerHTML = userMoney;
-resetButton.hidden = true;
+const bettingResultText = document.querySelector('.js_changeText');
+const playButton = document.querySelector('.js_btn');
+const resetButton = document.querySelector('.js_reset');
+const currentUserBalanceText = document.querySelector('.js_user_balance');
 
-// 2. Declare functions and events
+const minBalance = 0;
+const maxBalance = 200;
+const userStartingBalance = 50;
+
+let currentUserBalance;
+
+function restartGame() {
+  numberSelect.value = 0;
+  bettingAmountInput.value = '';
+
+  currentUserBalance = userStartingBalance;
+  currentUserBalanceText.innerHTML = currentUserBalance;
+
+  playButton.classList.remove('hidden');
+  resetButton.classList.add('hidden');
+}
+
+function endGame() {
+  playButton.classList.add('hidden');
+  resetButton.classList.remove('hidden');
+}
+
+function onPlayButtonClick(event) {
+  event.preventDefault();
+  if (parseInt(numberSelect.value) && bettingAmountInput.value) {
+    doBet();
+    if (currentUserBalance <= minBalance || currentUserBalance >= maxBalance) {
+      endGame();
+    }
+  } else {
+    console.log('You cannot bet! Select a number and set an amount to bet');
+  }
+}
+
+function onResetClick(event) {
+  event.preventDefault();
+  restartGame();
+}
+
+function doBet() {
+  const bettingAmount = bettingAmountInput.value;
+  const userSelectedNumber = parseInt(numberSelect.value);
+  const randomNumber = getRandomNumber(6);
+  if (userSelectedNumber === randomNumber) {
+    currentUserBalance += bettingAmount * 2;
+    bettingResultText.innerHTML = 'Has ganado el doble de lo apostado :)';
+  } else {
+    currentUserBalance = Math.max(currentUserBalance - bettingAmount, 0);
+    bettingResultText.innerHTML = 'Has perdido lo apostado :(';
+  }
+  currentUserBalanceText.innerHTML = currentUserBalance;
+}
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-btn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const bet = moneyInput.value;
-  const playerSelectedNumber = parseInt(select.value);
-  const randomNumber = getRandomNumber(6);
-  if (playerSelectedNumber === randomNumber) {
-    userMoney += bet * 2;
-    resultText.innerHTML = "Has ganado el doble de lo apostado :)";
-  } else {
-    userMoney = Math.max(userMoney - bet, 0);
-    resultText.innerHTML = "Has perdido lo apostado :(";
-  }
+playButton.addEventListener('click', onPlayButtonClick);
+resetButton.addEventListener('click', onResetClick);
 
-  userMoneyText.innerHTML = userMoney;
-
-  if (userMoney <= 0 || userMoney >= 200) {
-    playButton.hidden = true;
-    resetButton.hidden = false;
-  }
-});
+// Starting the game
+restartGame();
